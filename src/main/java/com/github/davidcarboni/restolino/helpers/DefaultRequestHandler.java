a0493797b526dc72105cd9520d0b5603e3a7bda4
david <david@carboni.io>
2014-07-22 08:48:00 +0100

    Added default request handler for unimplemented HTTP methods on each endpoint.

---
+++ src/main/java/com/github/davidcarboni/restolino/helpers/DefaultRequestHandler.java
@@ -0,0 +1,31 @@
+package com.github.davidcarboni.restolino.helpers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpStatus;
+
+/**
+ * This request handler gets set as the default for each HTTP method on each
+ * endpoint.
+ * 
+ * @author david
+ *
+ */
+public class DefaultRequestHandler {
+
+	static String notImplemented = " is not available for ";
+
+	/**
+	 * Sets a status of {@link HttpStatus#SC_METHOD_NOT_ALLOWED}.
+	 * 
+	 * @param req
+	 *            {@link HttpServletRequest}
+	 * @param res
+	 *            {@link HttpServletResponse}
+	 */
+	public String notImplemented(HttpServletRequest req, HttpServletResponse res) {
+		res.setStatus(HttpStatus.SC_METHOD_NOT_ALLOWED);
+		return req.getMethod() + notImplemented + req.getPathInfo();
+	}
+}
