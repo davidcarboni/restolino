d6536e00fb524f3e8133a4eeaad8f6abfdec0876
david <david@carboni.io>
2014-09-06 21:40:09 +0100

    Rearranged for nice new ways of running.

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y"
+export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-export RESTOLINO_STATIC="./src/main/webapp"
-export RESTOLINO_CLASSES="./target/classes"
+export RESTOLINO_STATIC="src/main/resources/files"
+export RESTOLINO_CLASSES="target/classes"
 export RESTOLINO="-Drestolino.static=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES"
 
 mvn clean package && \
 #java $JAVA_OPTS $RESTOLINO -cp "target/dependency/*" -jar target/*.jar
+#java $JAVA_OPTS $RESTOLINO -cp "target/classes:target/dependency/*" com.github.davidcarboni.restolino.Main
 java $JAVA_OPTS $RESTOLINO -cp "target/classes:target/dependency/*" com.github.davidcarboni.restolino.Main
