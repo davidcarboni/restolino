347845682e21fdacbe8ca9f402bc09805746b27b
david <david@carboni.io>
2014-09-09 15:03:03 +0100

    Cleanup.

--- src/main/java/com/github/davidcarboni/restolino/handlers/FilesHandler.java
+++ src/main/java/com/github/davidcarboni/restolino/handlers/FilesHandler.java
@@ -1,10 +1,7 @@
 package com.github.davidcarboni.restolino.handlers;
 
-import java.io.IOException;
 import java.net.URL;
-import java.nio.file.FileSystems;
 
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.util.resource.Resource;
 
@@ -20,36 +17,22 @@ import com.github.davidcarboni.restolino.Configuration;
  */
 public class FilesHandler extends ResourceHandler {
 
-	static String KEY_FILES = "restolino.files";
 	static String filesResourceName = "files";
-	Configuration configuration;
+	static Configuration configuration;
 
-	FilesHandler(URL url, Configuration configuration) {
-		this.configuration = configuration;
+	FilesHandler(URL url) {
 		Resource base = Resource.newResource(url);
 		setBaseResource(base);
 	}
 
 	public static ResourceHandler newInstance(Configuration configuration) {
+		FilesHandler.configuration = configuration;
 		URL url = null;
 
 		// If the property is set, reload from a local directory:
-		if (StringUtils.isNotBlank(System.getProperty(KEY_FILES))) {
-
-			String path = System.getProperty(KEY_FILES);
-			if (StringUtils.isNotBlank(path)) {
-				try {
-					// Running with reloading:
-					url = FileSystems.getDefault().getPath(path).toUri()
-							.toURL();
-				} catch (IOException e) {
-					throw new RuntimeException("Error starting class reloader",
-							e);
-				}
-			}
-
+		if (configuration.filesReloadable) {
+			url = configuration.filesUrl;
 		} else {
-
 			// Check for a resource on the classpath (production):
 			url = MainHandler.class.getClassLoader().getResource(
 					filesResourceName);
@@ -59,7 +42,7 @@ public class FilesHandler extends ResourceHandler {
 		FilesHandler result = null;
 		if (url != null) {
 			System.out.println("Set up file handler for URL: " + url);
-			result = new FilesHandler(url, configuration);
+			result = new FilesHandler(url);
 		}
 		return result;
 	}
