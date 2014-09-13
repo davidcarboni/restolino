7046eec807501bd098b9dab188ce6f336d0ea7bf
David Carboni <david@carboni.io>
2014-09-13 17:02:52 +0100

    Removed unused Servlet configuration methods and updated Javadoc.

--- src/main/java/com/github/davidcarboni/restolino/Main.java
+++ src/main/java/com/github/davidcarboni/restolino/Main.java
@@ -1,84 +1,34 @@
 package com.github.davidcarboni.restolino;
 
-import javax.servlet.Servlet;
-
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.BaseHolder.Source;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.webapp.WebAppContext;
 
 import com.github.davidcarboni.restolino.jetty.MainHandler;
 
 /**
  * 
- * This class launches the web application in an embedded Jetty container. This
- * is the entry point to your application. The Java command that is used for
- * launching should fire this main method.
+ * This class launches an embedded Jetty server. This is the entry point to your
+ * application. The Java command lie you use should reference this class, or you
+ * can set it as the <code>Main-Class</code> in your JAR manifest.
  *
  */
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 
+		// Set up the configuration:
 		Configuration configuration = new Configuration();
 		System.out.println("Using port " + configuration.port
 				+ " (specify a PORT environment variable to change it)");
+
+		// Create the Jetty server:
 		Server server = new Server(configuration.port);
 		Handler mainHandler = new MainHandler(configuration);
 		server.setHandler(mainHandler);
 		server.start();
-		// server.dumpStdErr();
+
+		// And we're done.
 		server.join();
-	}
-
-	static int order = 0;
-
-	/**
-	 * Adds a servlet that will get initialised on startup.
-	 * 
-	 * @param root
-	 * @param servletClass
-	 * @param pathSpec
-	 */
-	static void addServlet(WebAppContext root,
-			Class<? extends Servlet> servletClass, String pathSpec) {
-		ServletHolder holder = new ServletHolder(Source.EMBEDDED);
-		holder.setClassName(servletClass.getName());
-		holder.setInitOrder(++order);
-		root.addServlet(holder, pathSpec);
-	}
-
-	/**
-	 * Adds a servlet that will get initialised on startup.
-	 * 
-	 * @param root
-	 * @param servletClass
-	 * @param pathSpec
-	 */
-	static void addServlet(ServletHandler root,
-			Class<? extends Servlet> servletClass, String pathSpec) {
-		ServletHolder holder = new ServletHolder(Source.EMBEDDED);
-		holder.setClassName(servletClass.getName());
-		holder.setInitOrder(++order);
-		root.addServletWithMapping(holder, pathSpec);
-	}
-
-	/**
-	 * Adds a servlet that will get initialised on startup.
-	 * 
-	 * @param root
-	 * @param servletClass
-	 * @param pathSpec
-	 */
-	static void addServlet(ServletContextHandler root,
-			Class<? extends Servlet> servletClass, String pathSpec) {
-		ServletHolder holder = new ServletHolder(Source.EMBEDDED);
-		holder.setClassName(servletClass.getName());
-		holder.setInitOrder(++order);
-		root.addServlet(holder, pathSpec);
 	}
 
 }
