a52ee28fe65690d18aa8b1cd8ae0aed3205b3009
David Carboni <david@carboni.io>
2014-09-27 11:07:35 +0100

    Cleaned up the run script a bit. It's still focused on running Restolino for Restolino development, so isn't a good example of how you'd run your own app.

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 
 export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-export RESTOLINO_STATIC="src/main/resources/files"
+export RESTOLINO_FILES="src/main/resources/files"
 export RESTOLINO_CLASSES="target/classes"
-export RESTOLINO="-Drestolino.static=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES"
 
 mvn clean package && \
-#java $JAVA_OPTS $RESTOLINO -cp "target/dependency/*" -jar target/*.jar
-#java $JAVA_OPTS $RESTOLINO -cp "target/classes:target/dependency/*" com.github.davidcarboni.restolino.Main
-java $JAVA_OPTS $RESTOLINO -cp "target/classes:target/dependency/*" com.github.davidcarboni.restolino.Main
+#java $JAVA_OPTS -Drestolino.files=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES -cp "target/dependency/*" -jar target/*.jar
+#java $JAVA_OPTS -Drestolino.files=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES -cp "target/classes:target/dependency/*" com.github.davidcarboni.restolino.Main
+java $JAVA_OPTS -Drestolino.files=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES -cp "target/classes:target/dependency/*" com.github.davidcarboni.restolino.Main
