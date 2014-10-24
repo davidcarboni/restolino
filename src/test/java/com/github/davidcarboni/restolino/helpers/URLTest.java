23047c10e760385275136ac9e0f867d0920ecec5
David Carboni <david@carboni.io>
2014-10-24 10:31:04 +0100

    Added URL helper class.

---
+++ src/test/java/com/github/davidcarboni/restolino/helpers/URLTest.java
@@ -0,0 +1,41 @@
+package com.github.davidcarboni.restolino.helpers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+public class URLTest {
+
+	String exampleSimple = "http://example.com";
+	String exampleFull = "https://user:password@example.com:8080?parameter=value#fragment";
+
+	@Test
+	public void shouldParseSimple() throws URISyntaxException {
+
+		// When
+		URL url = new URL(exampleSimple);
+
+		// Then
+		assertEquals("http", url.protocol);
+		assertEquals("example.com", url.host);
+	}
+
+	@Test
+	public void shouldParseFull() throws URISyntaxException {
+
+		// When
+		URL url = new URL(exampleFull);
+
+		// Then
+		assertEquals("https", url.protocol);
+		assertEquals("user:password", url.userInfo);
+		assertEquals("example.com", url.host);
+		assertEquals(8080, url.port);
+		assertEquals(1, url.query.size());
+		assertEquals("value", url.query.get("parameter"));
+		assertEquals("fragment", url.fragment);
+	}
+
+}
