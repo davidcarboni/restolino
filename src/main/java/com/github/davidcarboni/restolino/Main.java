f9c158ea1e1d38b3a4cbbb9e6b5ee33434e85473
David Carboni <david@carboni.io>
2015-06-01 16:19:50 +0100

    Added graceful shutdown using StatisticsHandler and a JVM shutdown hook. This allows time for requests to complete after requesting shutdown.

--- src/main/java/com/github/davidcarboni/restolino/Main.java
+++ src/main/java/com/github/davidcarboni/restolino/Main.java
@@ -4,58 +4,82 @@ import com.github.davidcarboni.restolino.jetty.BasicAuth;
 import com.github.davidcarboni.restolino.jetty.MainHandler;
 import com.github.davidcarboni.restolino.reload.ClassReloader;
 import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.StatisticsHandler;
 import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 
 /**
- * 
  * This class launches an embedded Jetty server. This is the entry point to your
  * application. The Java command lie you use should reference this class, or you
  * can set it as the <code>Main-Class</code> in your JAR manifest.
- *
  */
 public class Main {
 
-	public static Configuration configuration;
-	public static Server server;
+    public static Configuration configuration;
+    public static Server server;
 
-	// Handlers
-	public static MainHandler mainHandler;
-	public static GzipHandler gzipHandler;
-	public static SecurityHandler securityHandler;
+    // Handlers
+    public static MainHandler mainHandler;
+    public static GzipHandler gzipHandler;
+    public static SecurityHandler securityHandler;
+    /**
+     * The statistics handler provides graceful shutdown.
+     */
+    public static StatisticsHandler statisticsHandler;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		try {
-			// Set up the configuration:
-			configuration = new Configuration();
+        try {
+            // Set up the configuration
+            configuration = new Configuration();
 
-			// Create the Jetty server:
-			server = new Server(configuration.port);
+            // Create the Jetty server
+            server = new Server(configuration.port);
 
-			// Create the handlers
-			mainHandler = new MainHandler();
-			gzipHandler = new GzipHandler();
-			gzipHandler.setHandler(mainHandler);
+            // Create the handlers
+            mainHandler = new MainHandler();
+            gzipHandler = new GzipHandler();
+            gzipHandler.setHandler(mainHandler);
 
-			// Select the handler to be used:
-			if (configuration.authenticationEnabled) {
-				securityHandler = new BasicAuth();
-				securityHandler.setHandler(gzipHandler);
-				server.setHandler(securityHandler);
-			} else {
-				server.setHandler(gzipHandler);
-			}
+            // Select the handler to be used
+            Handler handler;
+            if (configuration.authenticationEnabled) {
+                securityHandler = new BasicAuth();
+                securityHandler.setHandler(gzipHandler);
+                handler = securityHandler;
+            } else {
+                handler = gzipHandler;
+            }
 
-			// And we're done.
-			server.start();
-			System.out.println(configuration);
-			System.out.println("\nCompleted startup process.");
-			server.join();
+            // Graceful shutdown
+            statisticsHandler = new StatisticsHandler();
+            statisticsHandler.setHandler(handler);
+            server.setHandler(statisticsHandler);
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("Initiating graceful shutdown...");
+                        //server.getHandler().stop();
+                        server.stop();
+                        System.out.println("Shutdown completed gracefully.");
+                    } catch (Exception e) {
+                        System.out.println("Shutdown error:");
+                        e.printStackTrace();
+                    }
+                }
+            }));
 
-		} finally {
-			ClassReloader.shutdown();
-		}
-	}
+            // And we're good to go
+            server.start();
+            System.out.println(configuration);
+            System.out.println("\nCompleted startup process.");
+            server.join();
+
+        } finally {
+            ClassReloader.shutdown();
+        }
+    }
 
 }
