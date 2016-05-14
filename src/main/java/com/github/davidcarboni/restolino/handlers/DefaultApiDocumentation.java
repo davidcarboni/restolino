b849ac66bbfdb35b8c4a7801e0bca3777d995520
David Carboni <david@carboni.io>
2016-05-14 22:24:05 +0100

    Modified to return the ApiConfiguration to show what URIs and HTTP methods are available.

--- src/main/java/com/github/davidcarboni/restolino/handlers/DefaultApiDocumentation.java
+++ src/main/java/com/github/davidcarboni/restolino/handlers/DefaultApiDocumentation.java
@@ -1,19 +1,19 @@
 package com.github.davidcarboni.restolino.handlers;
 
-import com.github.davidcarboni.restolino.Configuration;
-import com.github.davidcarboni.restolino.Main;
+import com.github.davidcarboni.restolino.api.ApiConfiguration;
 import com.github.davidcarboni.restolino.framework.Home;
+import com.github.davidcarboni.restolino.jetty.ApiHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Default {@link Home} handler.
+ * Default {@link Home} handler that prints out the API configuration (URIs, methods and messages).
  */
 public class DefaultApiDocumentation implements Home {
     @Override
-    public Configuration get(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        return Main.configuration;
+    public ApiConfiguration get(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        return ApiHandler.api;
     }
 }
