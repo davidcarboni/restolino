f09cffd9cb8154930e2151d9fc299574e9025c3a
David Carboni <david@carboni.io>
2016-05-16 10:34:40 +0100

    Improving class name abbreviation.

--- src/main/java/com/github/davidcarboni/restolino/serialisers/ClassNameSerialiser.java
+++ src/main/java/com/github/davidcarboni/restolino/serialisers/ClassNameSerialiser.java
@@ -8,31 +8,30 @@ import org.apache.commons.lang3.StringUtils;
  */
 public abstract class ClassNameSerialiser {
 
+    /** For abbreviating class names like String and Integer. */
     static final String LANG_PACKAGE = "java.lang.";
+
+    /** For abbreviating class names like List, Map and Set. */
+    static final String UTIL_PACKAGE = "java.util.";
+
+    /** For abbreviating request and response class names. */
     static final String HTTP_PACKAGE = "javax.servlet.http.";
 
     protected String className(Class<?> clazz) {
         String result = clazz.getName();
-
-        // Remove "java.lang" - for things like String and Integer:
-        if (StringUtils.startsWith(result, LANG_PACKAGE)) {
-            result = StringUtils.replace(result, LANG_PACKAGE, "");
+        if (StringUtils.startsWith(result, ".")) {
+            result = StringUtils.substring(result, 1);
         }
+        return removePrefixes(result, LANG_PACKAGE, UTIL_PACKAGE, HTTP_PACKAGE, Main.configuration.packagePrefix);
+    }
 
-        // Remove "javax.servlet.http" - for request and response objects:
-        if (StringUtils.startsWith(result, HTTP_PACKAGE)) {
-            result = StringUtils.replace(result, HTTP_PACKAGE, "");
-        }
-
-        // Remove the package prefix, if configured:
-        String packagePrefix = Main.configuration.packagePrefix;
-        if (StringUtils.isNotBlank(packagePrefix)) {
-            if (!StringUtils.endsWith(packagePrefix, ".")) {
-                packagePrefix += ".";
+    private String removePrefixes(String className, String... prefixes) {
+        String result = className;
+        for (String prefix : prefixes) {
+            if (StringUtils.isNotBlank(prefix) && StringUtils.startsWith(result, LANG_PACKAGE)) {
+                result = StringUtils.replace(result, LANG_PACKAGE, "");
             }
-            result = StringUtils.replace(result, packagePrefix, "");
         }
-
         return result;
     }
 }
